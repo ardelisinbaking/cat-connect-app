756659db9e1b77dca3e614cb2b4cee2bf3f5f0b7
Ardelis Yeung <oyu.ardelis@gmail.com>
2024-11-24 22:45:25 -0800

    update prisma configuration and verify local API

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,9 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined
 }
   
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient()
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
