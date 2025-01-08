031af8a08d57200bb0c07474aa39075a16f3ad08
Ardelis Yeung <oyu.ardelis@gmail.com>
2025-01-08 15:40:45 -0800

    revert page out of test script

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
+import LandingPage from '@/components/LandingPage'
+
 export default function Home() {
-  return <div>Hello World Test</div>
+  console.log('Rendering Home Page');
+  return <LandingPage />
 }
-
-
-// import LandingPage from '@/components/LandingPage'
-
-// export default function Home() {
-//   console.log('Rendering Home Page');
-//   return <LandingPage />
-// }
