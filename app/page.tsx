f4b3d83679ea356bcd63b5dc37332208f7be2e80
Ardelis Yeung <oyu.ardelis@gmail.com>
2025-01-08 15:33:33 -0800

    test page to check routing or component issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import LandingPage from '@/components/LandingPage'
-
 export default function Home() {
-  return <LandingPage />
+  return <div>Hello World Test</div>
 }
+
+
+// import LandingPage from '@/components/LandingPage'
+
+// export default function Home() {
+//   console.log('Rendering Home Page');
+//   return <LandingPage />
+// }
